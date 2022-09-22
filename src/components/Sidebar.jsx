66e424481ea4aa7eb8266e19e9516632a72a6086
Olivier Belzeaux <olivierbelzeaux@gmail.com>
2022-09-22 21:28:54 +0200

    Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,14 @@ import { useStateContext } from '../contexts/ContextProvider.js';
 import { itemsToOrder } from '@syncfusion/ej2/treemap';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
+
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray text-md m-2';
 
@@ -26,7 +33,7 @@ const Sidebar = () => {
           <TooltipComponent content="Menu" position="BottomCenter">
             <button
               type='button'
-              onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+              onClick={handleCloseSidebar}
               className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
               <MdOutlineCancel/>
             </button>
@@ -42,7 +49,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${Link.name}`}
                   key={Link.name}
-                  onClick={() => {}}
+                  onClick={handleCloseSidebar}
                   className={({ isActive }) => isActive ? activeLink : normalLink}
                 >
                   {Link.icon}
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -2,7 +2,7 @@ import React,{ createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
-const initiaState = {
+const initialState = {
   chat: false,
   cart: false,
   userProfile: false,
@@ -12,11 +12,24 @@ const initiaState = {
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
 
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true});
+  }
+
+  const [screenSize, setScreenSize] = useState(undefined)
+
   return (
     <StateContext.Provider
     value={{
       activeMenu,
       setActiveMenu,
+      isClicked,
+      setIsClicked,
+      handleClick,
+      screenSize,
+      setScreenSize,
      }}>
       {children}
     </StateContext.Provider>
