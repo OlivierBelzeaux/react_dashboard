1c2764522db8d053b5e2c5865e130be754fadbc0
Olivier Belzeaux <olivierbelzeaux@gmail.com>
2022-09-27 21:08:00 +0200

    theme fixes

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,83 +1,98 @@
-import React from 'react';
-import { BsCurrencyDollar } from 'react-icons/bs';
-import { GoPrimitiveDot } from 'react-icons/go';
-import { IoIosMore } from 'react-icons/io';
-import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { IoIosMore } from "react-icons/io";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 
-import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
-import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
-import product9 from '../data/product9.jpg';
+import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
+import {
+  earningData,
+  medicalproBranding,
+  recentTransactions,
+  weeklyStats,
+  dropdownData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+import product9 from "../data/product9.jpg";
 
 const Ecommerce = () => {
+  const { currentColor, currentMode } = useStateContext();
+
   return (
-    <div className='mt-12'>
-      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
-          <div className='flex justify-between items-cente'>
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3">
+          <div className="flex justify-between items-center">
             <div>
-              <p className='font-bold text-gray-400'>Earnings</p>
-              <p className='text-2xl'>98 898 234 €</p>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="text-2xl">98 898 234 €</p>
             </div>
           </div>
-          <div className='mt-6'>
+          <div className="mt-6">
             <Button
               color="white"
-              bgColor='blue'
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
-              size='md'
+              size="md"
             />
           </div>
         </div>
 
-        <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
-          { earningData.map((item) => (
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
             <div
-            key={item.title}
-            className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'>
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+            >
               <button
-              type='button'
-              style={{ color:item.iconColor, backgroundColor: item.iconBg }}
-              className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
               >
                 {item.icon}
               </button>
-              <p className='mt-3'>
-                <span className='text-lg font-semibold'>
-                  {item.amount}
-                </span>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
                 <span className={`text-sm text-${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
               </p>
-              <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
         </div>
       </div>
 
-      <div className='flex gap-10 flex-wrap justify-center'>
-        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
-          <div className='flex justify-between'>
-            <p className='font-semibold text-xl'>Revenue Updates</p>
-            <div className='flex items-center gap-4'>
-              <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
-                <span><GoPrimitiveDot/></span>
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
                 <span>Expense</span>
               </p>
-              <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
-                <span><GoPrimitiveDot/></span>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
                 <span>Budget</span>
               </p>
             </div>
           </div>
-          <div className='mt-10 flex gap-10 flew-wrap justify-center'>
-            <div className='border-r-1 border-color m-4 pr-10'>
+          <div className="mt-10 flex gap-10 flew-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
               <div>
                 <p>
-                  <span className='text-3xl font-semibold'>93,564€</span>
-                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>25%</span>
+                  <span className="text-3xl font-semibold">93,564€</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    25%
+                  </span>
                 </p>
                 <p className="text-gray-500 mt-1">Budget</p>
               </div>
@@ -86,12 +101,20 @@ const Ecommerce = () => {
                 <p className="text-gray-500 mt-1">Expense</p>
               </div>
               <div className="mt-5">
-                <SparkLine currentColor="blue" id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
+                <SparkLine
+                  currentColor={currentColor}
+                  id="line-sparkLine"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color={currentColor}
+                />
               </div>
               <div className="mt-10">
                 <Button
                   color="white"
-                  bgColor="blue"
+                  bgColor={currentColor}
                   text="Download Report"
                   borderRadius="10px"
                 />
@@ -103,9 +126,8 @@ const Ecommerce = () => {
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Ecommerce
+export default Ecommerce;
